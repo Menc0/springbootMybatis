a3a300909429fc422bec7c656d887cb4647c1fad
cwh <cwh.1010@qq.com>
2018-02-01 17:26:12 +0800

    验证连接池

--- src/main/java/com/cwh/springbootMybatis/Application.java
+++ src/main/java/com/cwh/springbootMybatis/Application.java
@@ -1,12 +1,25 @@
 package com.cwh.springbootMybatis;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner{
 
+	@Autowired
+    DataSource dataSource;
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	
+	/**
+	 * 查看用了什么数据库连接池
+	 */
+	public void run(String... args) throws Exception {
+        System.out.println("DATASOURCE = " + dataSource);
+    }
 }
